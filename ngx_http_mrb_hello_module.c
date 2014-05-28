57715769ea53bcb6071068164ee70ee8d4d2f90a
Liu Lantao <liulantao@gmail.com>
2014-05-28 18:17:09 +0800

    Init ngx_http_mrb_hello_module
    Embed the Power of mruby into NGINX

---
+++ ngx_http_mrb_hello_module.c
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) Liu Lantao, https://github.com/Lax
+ */
+
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+#include <string.h>
+#include "mruby.h"
+#include "mruby/compile.h"
+#include "mruby/string.h"
+
+static char *ngx_http_mrb_hello(ngx_conf_t *cf, ngx_command_t *cmd,
+    void *conf);
+
+static ngx_command_t ngx_http_mrb_hello_commands[] = {
+    { ngx_string("hello"),
+      NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS,
+      ngx_http_mrb_hello,
+      0,
+      0,
+      NULL },
+
+    ngx_null_command
+};
+
+
+u_char *ngx_hello_string = (u_char *)"Hello, world!";
+
+
+static ngx_http_module_t ngx_http_mrb_hello_module_ctx = {
+    NULL,                          /* preconfiguration */
+    NULL,                          /* postconfiguration */
+
+    NULL,                          /* create main configuration */
+    NULL,                          /* init main configuration */
+
+    NULL,                          /* create server configuration */
+    NULL,                          /* merge server configuration */
+
+    NULL,                          /* create location configuration */
+    NULL                           /* merge location configuration */
+};
+
+
+ngx_module_t ngx_http_mrb_hello_module = {
+    NGX_MODULE_V1,
+    &ngx_http_mrb_hello_module_ctx,    /* module context */
+    ngx_http_mrb_hello_commands,       /* module directives */
+    NGX_HTTP_MODULE,               /* module type */
+    NULL,                          /* init master */
+    NULL,                          /* init module */
+    NULL,                          /* init process */
+    NULL,                          /* init thread */
+    NULL,                          /* exit thread */
+    NULL,                          /* exit process */
+    NULL,                          /* exit master */
+    NGX_MODULE_V1_PADDING
+};
+
+
+static ngx_int_t
+ngx_http_mrb_hello_handler(ngx_http_request_t *r)
+{
+    ngx_int_t    rc;
+    ngx_buf_t   *b;
+    ngx_chain_t  out;
+
+    /* we response to 'GET' and 'HEAD' requests only */
+    if (!(r->method & (NGX_HTTP_GET|NGX_HTTP_HEAD))) {
+        return NGX_HTTP_NOT_ALLOWED;
+    }
+
+    /* discard request body, since we don't need it here */
+    rc = ngx_http_discard_request_body(r);
+
+    if (rc != NGX_OK) {
+        return rc;
+    }
+
+    mrb_state *mrb = mrb_open();
+    mrbc_context *cxt = mrbc_context_new(mrb);
+    mrb_value v;
+    int ngx_hello_string_len;
+
+    v = mrb_load_string_cxt(mrb, "config=\"Current Time [#{Time.now}]\"; config;", cxt);
+    ngx_hello_string = (u_char *)mrb_str_to_cstr(mrb, v);
+    ngx_hello_string_len = mrb_fixnum(mrb_funcall(mrb, v, "size", 0, 0));
+
+    /* set the 'Content-type' header */
+    r->headers_out.content_type_len = sizeof("text/html") - 1;
+    r->headers_out.content_type.len = sizeof("text/html") - 1;
+    r->headers_out.content_type.data = (u_char *) "text/html";
+
+    /* send the header only, if the request type is http 'HEAD' */
+    if (r->method == NGX_HTTP_HEAD) {
+        r->headers_out.status = NGX_HTTP_OK;
+        r->headers_out.content_length_n = ngx_hello_string_len;
+
+        return ngx_http_send_header(r);
+    }
+
+    /* allocate a buffer for your response body */
+    b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+    if (b == NULL) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    /* attach this buffer to the buffer chain */
+    out.buf = b;
+    out.next = NULL;
+
+    /* adjust the pointers of the buffer */
+    b->pos = ngx_hello_string;
+    b->last = ngx_hello_string + ngx_hello_string_len;
+    b->memory = 1;    /* this buffer is in memory */
+    b->last_buf = 1;  /* this is the last buffer in the buffer chain */
+
+    /* set the status line */
+    r->headers_out.status = NGX_HTTP_OK;
+    r->headers_out.content_length_n = ngx_hello_string_len;
+
+    /* send the headers of your response */
+    rc = ngx_http_send_header(r);
+
+    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
+        return rc;
+    }
+
+    /* send the buffer chain of your response */
+    return ngx_http_output_filter(r, &out);
+}
+
+
+static char *
+ngx_http_mrb_hello(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_core_loc_conf_t *clcf;
+
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+    clcf->handler = ngx_http_mrb_hello_handler; /* handler to process the 'hello' directive */
+
+    return NGX_CONF_OK;
+}
